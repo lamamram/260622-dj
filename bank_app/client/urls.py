119cd1886ae58e795eb43639cafcdb35f9c68243
mlamamra <mlamamra@dawan.fr>
2026-06-22 17:49:48 +0200

    add theme

--- bank_app/client/urls.py
+++ bank_app/client/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 # "." chemin relatif à partir du fichier courant
 from . import views
 
-app_name = "client"
 urlpatterns = [
     # chemin relatif à partir du radical de l'app client
     # charger la vue qui est une fonction dans le module views
-    path("home", views.home)
+    path("home", views.home, name="home")
 ]
