3b5441e5df25db628a06b6080c723dbfa78a05f2
mlamamra <mlamamra@dawan.fr>
2026-06-23 12:08:07 +0200

    migration issue with current_timstamp for datetime field with sqlite and django

--- bank_app/client/views.py
+++ bank_app/client/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Client
 
 # Create your views here.
 # première vue : vue-fonction
 def home(request):
-    # créer le sous dossier templates dans l'app client et y insérer home.html
-    return render(request, "home.html")
+    # accès statique (pas d'instanciation) avec l'attribut .objects
+    # qui contient les méthodes de requêtages sur la table
+    client = Client.objects.get(id=1)
+    # 1. créer le sous dossier templates dans l'app client et y insérer home.html
+    # 2. ajouter le contexte pour injecter les objets python dans le template
+    return render(request, "home.html", context={
+        # c'est la clé qui donne le nom à l'objet dans le template !!!
+        "client": client
+    })
